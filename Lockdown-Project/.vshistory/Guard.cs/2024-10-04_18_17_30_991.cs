e6487b5d41f05adb2c1c5596e052776c634c0ff7
Cedric Guessou <cdd8606@rit.edu>
2024-10-04 21:47:33 -0400

    Guard started

---
+++ Lockdown-Project/.vshistory/Guard.cs/2024-10-04_18_17_30_991.cs
@@ -0,0 +1,22 @@
+using Godot;
+using System;
+
+public partial class Guard : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		va
+		
+	}
+
+	public override void _Physics(double delta)
+	{
+		
+	}
+}
---
+++ Lockdown-Project/Guard.cs
@@ -0,0 +1,27 @@
+using Godot;
+using System;
+
+public partial class Guard : Node2D
+{
+	[Export] public int stamina;
+	
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+	}
+
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+		GlobalPosition += new Vector2(10, 10) * (float)delta;
+
+		if (Input.IsKeyPressed(Key.W))
+		{
+			GlobalPosition += new Vector2(0, -50) * (float)delta;
+		}
+
+		if (Input.IsKeyPressed(Key.S))
+		{
+		}
+	}
+}
