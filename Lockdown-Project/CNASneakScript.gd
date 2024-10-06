55ec5488cf507e57671b10022395f45c500ba4ab
Daniel Murphy (RIT Student) <dtm6103@rit.edu>
2024-10-06 00:30:41 -0400

    Did a bunch more stuff with the Cyber Ninja's AI

--- Lockdown-Project/CNASneakScript.gd
+++ Lockdown-Project/CNASneakScript.gd
@@ -1,13 +1,21 @@
 extends State
 class_name CNASneak
 
-
 func enter() -> void:
-	#find a path to the objective (which avoids cameras and soldiers?)
-	navigationAgent.target_position = get_global_mouse_position()
-	cyberNinja.ninjaCurrentAction = "WALK"
+	get_node("../../").goalReached.connect(goal_reached)
+	#replace the mouse position with a function to locate the goal node's global position
+	goalCoords = get_global_mouse_position()
+	desiredAction = "WALK"
 	#connect to a signal to tell you if you've been spotted
-	
+	#this should come from a guard, but for now it comes from the ninja's body
+	get_node("../../").ninjaSpotted.connect(ninja_spotted)
+
+func goal_reached() -> void:
+	if self == get_node("../").currentState:
+		#replace this with a function to start hacking the door
+		print("this is where I'd start hacking the door")
+		desiredAction = "HACK"
+
 #func update(delta: float) -> void:
 	#if you recieve the signal to tell you you've been spotted:
 		#if stamina > 25:
@@ -19,6 +27,9 @@ func enter() -> void:
 			#hack the camera
 	#if you see a camera or a guard vision cone:
 		#pick a new path to avoid the vision area
+		
+func ninja_spotted() -> void:
+	transition.emit(self, "fight")
 
 func physics_update(delta: float) -> void:
 	pass
