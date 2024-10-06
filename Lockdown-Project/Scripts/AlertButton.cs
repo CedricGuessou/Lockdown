4090c448a19b5568afa51a85438b68b6d746ae04
Julian Howard (RIT Student) <jah2305@rit.edu>
2024-10-05 21:08:32 -0400

    Updated the scale marker to be more accessible at different zoom levels.

--- Lockdown-Project/Scripts/AlertButton.cs
+++ Lockdown-Project/Scripts/AlertButton.cs
@@ -23,15 +23,15 @@ public partial class AlertButton : Button
 		Vector2 zoom;
 		zoom = camera.Zoom;
 		Scale = new Vector2(1 / zoom.X, 1 / zoom.Y);
-		Vector2 screenSize = GetViewportRect().Size;
+		Vector2 screenSize = GetViewportRect().Size * Scale;
 		Rect2 camBounds = new Rect2(camera.GlobalPosition - screenSize / 2, screenSize);
 		g_pos = guard.Position;
 		
 		if(!camBounds.HasPoint(guard.GlobalPosition))
 		{
 			Vector2 alert_pos = g_pos;
-			alert_pos.X = Mathf.Clamp(alert_pos.X, camBounds.Position.X, camBounds.Position.X + camBounds.Size.X - 140);
-			alert_pos.Y = Mathf.Clamp(alert_pos.Y, camBounds.Position.Y, camBounds.Position.Y + camBounds.Size.Y - 140);
+			alert_pos.X = Mathf.Clamp(alert_pos.X, camBounds.Position.X + 70, camBounds.Position.X + camBounds.Size.X - 210);
+			alert_pos.Y = Mathf.Clamp(alert_pos.Y, camBounds.Position.Y + 70, camBounds.Position.Y + camBounds.Size.Y - 210);
 			this.Position = alert_pos;
 		}
 		else
