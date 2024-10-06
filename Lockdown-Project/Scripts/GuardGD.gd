68a4bceab2eaffc70ceb528d437e63b04849c9d8
Joshua DeWinter <jsd3713@rit.edu>
2024-10-05 22:44:51 -0400

    Add Selectable Guard

--- Lockdown-Project/Scripts/GuardGD.gd
+++ Lockdown-Project/Scripts/GuardGD.gd
@@ -12,6 +12,7 @@ var selected: bool = false
 var agent: NavigationAgent2D 
 
 var animPlayer: AnimationPlayer 
+var highlightPlayer: AnimationPlayer 
 
 var guardState: GuardState = GuardState.IDLE
 
@@ -27,24 +28,36 @@ enum GuardState
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	animPlayer = get_node("AnimationPlayer")
+	highlightPlayer = get_node("HighlightPlayer")
 	agent = get_node("NavigationAgent2D")
 	targetPosition = global_position
 
-
 func _input(event):
-	if (event is InputEventMouseButton && !event.pressed && event.button_index == MOUSE_BUTTON_RIGHT && guardState == GuardState.MOVE):
+	if (event is InputEventMouseButton && !event.pressed && event.button_index == MOUSE_BUTTON_RIGHT):
+		selected = false
+		highlight.visible = false
+	
+	if (event is InputEventMouseButton && !event.pressed && event.button_index == MOUSE_BUTTON_LEFT && guardState == GuardState.MOVE && selected):
 		var map: RID = get_world_2d().get_navigation_map()
 		var p = NavigationServer2D.map_get_closest_point(map, event.position)
 		agent.target_position = get_global_mouse_position()
 
 func selectedF():
+	var guards = get_tree().get_nodes_in_group("Guards")
+	for guard in guards:
+		if guard.selected:
+			guard.selected = false
+			guard.highlight.visible = false
 	selected = true
 	highlight.visible = true
+	highlightPlayer.play("GuardPokemon/SelectedIdle")
+	guardState = GuardState.MOVE
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
 	if (guardState == GuardState.MOVE):
 		if (agent.is_navigation_finished()):
+			animPlayer.play("GuardPokemon/RESET")
 			return
 
 		var diff: Vector2 = agent.get_next_path_position() - global_position
